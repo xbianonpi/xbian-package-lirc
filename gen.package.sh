eae920628743e9e6a4b0aaaac7e60d833e2f7789
Matus Kral <matuskral@me.com>
2013-05-09 22:28:50 +0200

    1.4 package

---
+++ gen.package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+package=$(cat ./content/DEBIAN/control | grep Package | awk '{print $2}')
+version=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}')
+
+fakeroot find ./content  | grep -v DEBIAN/ | xargs md5sum > ./content/DEBIAN/md5sums 
+fakeroot dpkg-deb -b ./content "${package}"-"${version}".deb
